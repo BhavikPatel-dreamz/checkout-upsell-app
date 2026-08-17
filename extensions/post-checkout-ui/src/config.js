7f20ba7291141a07be8a25e9f69c794bb8a359f2
Bhavik Patel <bhavik@dynamicdreamz.com>
2026-08-17 15:56:55 +0530

    remove extantions

--- extensions/post-checkout-ui/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    api_base_url : process.env.UPSELL_APP_URL,  
-    shopify_graphql_api_version: process.env.SHOPIFY_GRAPHQL_VERSION
-}
-
-export default config;
--- extensions/pre-checkout-ui/src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    api_base_url : "https://raq.dynamicdreamz.com/UpSell/api",
-    shopify_graphql_api_version: "2023-07"
-}
-
-export default config;
-
-
